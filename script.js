61c160650cf53365a97311234a2d8ca408e17771
thisistallia <talgorm00@gmail.com>
2024-04-20 15:47:49 -0700

    step 3, setting up a hover effect

--- script.js
+++ script.js
@@ -1,13 +1,16 @@
 document.addEventListener("DOMContentLoaded", function() {
-const container = document.getElementById('container');
-const totalDivs = 256;
+    const container = document.getElementById('container');
+    const totalDivs = 256;
 
 
-for (let i = 0; i < totalDivs; i++) {
-    let div = document.createElement('div');
-    div.classList.add('square');
-    container.appendChild(div);
+    for (let i = 0; i < totalDivs; i++) {
+        let div = document.createElement('div');
+        div.classList.add('square');
+        div.addEventListener('mouseover', function() {
+            this.style.backgroundColor = '#666';
+        });
+        container.appendChild(div);
 
-}
+    }
 });
 
